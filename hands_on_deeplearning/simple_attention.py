03a1ccdd23cac40f14eb1f04bbc7ac73129f1dfd
ChengXiao686 <xiao.cheng@autra.tech>
2024-03-29 16:57:33 +0800

    add attention

---
+++ hands_on_deeplearning/simple_attention.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleAttention(nn.Module):
+    def __init__(self, hidden_dim, num_heads):
+        self.hidden_dim = hidden_dim
+        self.num_heads =num_heads
+        self.head_dim = hidden_dim // num_heads
+        assert (self.head_dim * num_heads == hidden_dim), "must be divisible"
+        self.Wq = nn.Linear(hidden_dim, hidden_dim)
+        self.Wk = nn.Linear(hidden_dim, hidden_dim)
+        self.Wv = nn.Linear(hidden_dim, hidden_dim)
+        self.post_norm = nn.LayerNorm(hidden_dim)
+        self.fc_out = nn.Linear(hidden_dim, hidden_dim)
+
+    def _create_look_ahead_mask(self, size):
+        mask = torch.ones(size, size).triu(diagonal=1)
+        return mask
+    
+    def _scale_dot_attention(self, q, k, v, mask):
+        qk = torch.matmul(q, k)
+        scale = torch.sqrt(torch.tensor(self.head_dim, dtype=torch.float32))
+        logits = qk / scale
+        if mask:
+            logits += mask * 1e-9
+        attention_weights = F.softmax(logits, dim=-1)
+        output = torch.matmul(attention_weights, v)
+        return output
+
+    def forward(self, x, mask):
+        batch_size = x.shape[0]
+        sequence_length = x.shape[1]
+        if mask == None:
+            mask = self._create_look_ahead_mask(sequence_length)
+        Q = self.Wq(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        K = self.Wk(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        V = self.Wv(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        self.attention_output = self._scale_dot_attention(Q, K, V, mask).transpose(1, 2).contiguous().view(batch_size, -1, self.hidden_dim)
+        normed_output = self.post_norm(self.attention_output + x)
+        output = self.fc_out(normed_output)
+        return output
